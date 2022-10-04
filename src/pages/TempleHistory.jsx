a97649fb1a1ebdf5215e92adb05f8f36fa84dcb7
Vivek <vivek.k@ekoinfomatics.com>
2022-10-04 13:55:23 +0530

    vm v2 completed..

--- src/pages/TempleHistory.jsx
+++ src/pages/TempleHistory.jsx
@@ -1,11 +1,12 @@
 // react
 import React from "react";
+
+// Components
 import SecondSection from "../components/templeHistory/SecondSection";
 import FirstSection from "../components/templeHistory/FirstSection";
 import ThirdSection from "../components/templeHistory/ThirdSection";
 import FourthSection from "../components/templeHistory/FourthSection";
 import FifthSection from "../components/templeHistory/FifthSection";
-// Components
 
 const TempleHistory = () => {
   return (
